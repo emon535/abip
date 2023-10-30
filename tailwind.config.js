6ef465cf12ef2495af4d4eced51cdc60a73c0417
Md. Mehedi Hasan <mehedihasan47341@gmail.com>
2023-10-30 23:02:28 +0600

    tailwind setup done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+
+
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      height:{
+        'nav':'60px'
+      },
+      colors: {
+        'dark-blue-1': '#00284d',
+        'dark-blue-2': '#001a33',
+      }
+    },
+  },
+  plugins: [],
+}
+
