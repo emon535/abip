524605fc886dc253dc54da983c9f9fa241ee14d8
Md. Mehedi Hasan <mehedihasan47341@gmail.com>
2023-11-01 22:37:12 +0600

    member dashboard done

---
+++ src/components/MemberNav.jsx
@@ -0,0 +1,37 @@
+import { Link, useNavigate } from "react-router-dom";
+import { FaPowerOff } from "react-icons/fa6";
+import API from "../Api/api";
+import { memberUrl } from "../url";
+import useAuth from "../hooks/useAuth";
+function MemberNav(){
+    const { id } = useAuth()
+    const navigate = useNavigate()
+
+    function logOut(){
+        API.delete(`${memberUrl}/logOut/${id}`)
+        localStorage.removeItem("user");
+        navigate("/")
+    }
+
+
+    return(
+        <nav className=" w-full h-[70px] sticky top-0 z-20 flex items-center justify-between  bg-slate-300 px-20">
+            <div  className=" w-[200px] h-full flex items-center justify-center">
+                <Link to="/"><span className=" text-3xl uppercase">LOGO Here</span></Link> 
+            </div>
+            <div className=" w-[600px] h-full flex items-center gap-4 ">
+                {/* <NavLink to="/" className={({isActive})=>` uppercase ${isActive? 'text-orange-600':'text-slate-800'} `}>Home</NavLink>
+                <NavLink to="/about" className={({isActive})=>` uppercase ${isActive? 'text-orange-600':'text-slate-800'} `}>About Us</NavLink>
+                <NavLink to="/contact" className={({isActive})=>` uppercase ${isActive? 'text-orange-600':'text-slate-800'} `}>Contact Us</NavLink> */}
+            </div>
+            <div className=" w-[180px] ">
+                    <button onClick={logOut} className=" py-2 px-2 rounded-md flex items-center justify-center bg-red-600 text-slate-200"> 
+                        <FaPowerOff className=" text-2xl mr-1"/> 
+                        <span> Log Out</span>
+                    </button>
+            </div>
+        </nav>
+    )
+}
+
+export default MemberNav;
---
+++ src/pages/MemberDashboard.jsx
@@ -0,0 +1,12 @@
+import MemberNav from "../components/MemberNav";
+
+
+function MemberDashboard(){
+    return(
+        <div>
+            <MemberNav/>
+        </div>
+    )
+}
+
+export default MemberDashboard;
